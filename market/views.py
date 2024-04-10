57faebae56ce1bcb940e83f0865d566c1d8b5243
inavellevani <levan.tsopurashvili.1@btu.edu.ge>
2024-04-11 02:08:58 +0400

    pagination and detail page

--- market/views.py
+++ market/views.py
@@ -2,21 +2,38 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from market.models import Book
 from django.core import serializers
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def book_list(request):
     books = Book.objects.all()
-    data = serializers.serialize('json', books)
-    return JsonResponse(data)
+    paginator = Paginator(books, 3)
+    page_number = request.GET.get('page')
+
+    try:
+        page_obj = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.get_page(1)
+    except EmptyPage:
+        page_obj = paginator.get_page(paginator.num_pages)
+
+    context = {'books': page_obj}
+    if request.accepted_renderer.format == 'html':
+        return render(request, 'welcome.html', context)
+
+    data = serializers.serialize('json', page_obj)
+    return JsonResponse(data, safe=False)
 
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    data = serializers.serialize('json', book)
-    return JsonResponse(data)
+    context = {'book': book}
+    if request.accepted_renderer.format == 'html':
+        return render(request, 'book_detail.html', context)
+
+    data = serializers.serialize('json', [book])
+    return JsonResponse(data, safe=False)
 
 
 def welcome(request):
-    books = Book.objects.all()
-    context = {'books': books}
-    return render(request, 'welcome.html', context)
+    return book_list(request)
