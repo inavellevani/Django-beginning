06515748827c9b258eab06222fac12115856f12f
inavellevani <levan.tsopurashvili.1@btu.edu.ge>
2024-04-15 18:31:32 +0400

    updatin models

--- market/admin.py
+++ market/admin.py
@@ -1,10 +1,43 @@
 from django.contrib import admin
-from market.models import Book
+from django import forms
+from market.models import Book, Author, Category
+
+
+class BookForm(forms.ModelForm):
+    author_name = forms.ModelChoiceField(
+        queryset=Author.objects.all(),
+        widget=forms.TextInput(attrs={'placeholder': 'Enter author name'}),
+        to_field_name='name'
+    )
+    category = forms.ModelMultipleChoiceField(
+        queryset=Category.objects.all(),
+        widget=admin.widgets.FilteredSelectMultiple(verbose_name='Categories', is_stacked=False),
+        to_field_name='name'
+    )
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def clean_author_name(self):
+        name = self.cleaned_data['author_name']
+        author, created = Author.objects.get_or_create(name=name)
+        return author
+
+    def clean_category(self):
+        names = self.cleaned_data['category']
+        categories = []
+        for name in names:
+            category, created = Category.objects.get_or_create(name=name)
+            categories.append(category)
+        return categories
 
 
 class BooksAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author_name', 'category', 'price')
+    form = BookForm
+    list_display = ('name', 'author_name', 'price', 'cover_type')
     search_fields = ('name', 'author_name')
+    filter_horizontal = ('category',)
 
 
 admin.site.register(Book, BooksAdmin)
--- market/models.py
+++ market/models.py
@@ -1,15 +1,41 @@
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
+from market.genres import genre_choices
 import os
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Author's Name")
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        choices=genre_choices,
+        verbose_name="Category Name",
+
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
     name = models.CharField(max_length=100, verbose_name="Book Title")
     page_count = models.IntegerField(verbose_name="Number of Pages")
-    category = models.CharField(max_length=100, verbose_name="Category")
-    author_name = models.CharField(max_length=100, verbose_name="Author")
+    category = models.ManyToManyField(Category, verbose_name="Categories")
+    author_name = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books', verbose_name="Author")
     price = models.IntegerField(verbose_name="Price")
+    cover_type = models.CharField(
+        max_length=2,
+        choices=[('HC', 'Hardcover'), ('PB', 'Paperback'), ('SP', 'Special')],
+        default='HC',
+        verbose_name="Cover type"
+    )
     image = models.ImageField(upload_to='', blank=True, null=True, verbose_name="Book Cover")
 
     class Meta:
@@ -25,3 +51,4 @@ def delete_book_image(sender, instance, **kwargs):
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)
+
